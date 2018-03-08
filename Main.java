7692b4013cce7e8010742bfffc5c9dab485e7d90
ncpakl <35097287+ncpakl@users.noreply.github.com>
2018-03-08 14:52:55 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,5 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Some test code! Change to the message!");
+        System.out.println("Additional feature #1!");
     }
 }
