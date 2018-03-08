c0e0fc12df866d00acce0dc0b4ca7b8e5e7737eb
klatt.pawel <klatt.pawel@gmail.com>
2018-03-08 14:58:12 +0100

    Modified Feature #1

--- Main.java
+++ Main.java
@@ -4,6 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Some test code! Change to the message!");
-        System.out.println("Additional feature #1!");
+        System.out.println("Additional feature #1! Update to feature #1!");
     }
 }
