2dd14ce3c6b01c411c372d3d0278fa4e774fe45c
klatt.pawel <--global>
2018-03-09 12:38:58 +0100

    Implemented Feature #2

--- Main.java
+++ Main.java
@@ -6,4 +6,8 @@ public class Main {
         System.out.println("Some test code! Change to the message!");
         System.out.println("Additional feature #1! Update to feature #1!");
     }
+
+    public static void runStatic() {
+        System.out.println("Feature #2!");
+    }
 }
