0cad9531e599ed12d7ec855cdafbc6a3af736d01
klatt.pawel <klatt.pawel@gmail.com>
2018-03-09 13:02:11 +0100

    Team2 Feature #1

--- Main.java
+++ Main.java
@@ -3,7 +3,7 @@ package com.console;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Some test code! Change to the message!");
+        System.out.println("Some test code! Change to the message! Team2 change!");
         System.out.println("Additional feature #1! Update to feature #1!");
     }
 }
